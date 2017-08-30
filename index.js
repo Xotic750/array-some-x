4e45f3715c2e5c83769c653029f0695509e57f0c
Xotic750 <xotic750@gmail.com>
2017-08-31 01:33:15 +0200

    Add tests, tested IE8, Opera10.63 and FireFox4

--- index.js
+++ index.js
@@ -11,20 +11,72 @@
 
 var toObject = require('to-object-x');
 var assertIsFunction = require('assert-is-function-x');
+var documentElement = typeof document !== 'undefined' && document.documentElement;
 
-var tests = {
-  // Check node 0.6.21 bug where third parameter is not boxed
-  properlyBoxesNonStrict: true,
-  properlyBoxesStrict: true
-};
+var argObj = (function () {
+  return arguments;
+}(1));
 
-var nativeSome = Array.prototype.some;
+var nativeSome = typeof Array.prototype.some === 'function' && Array.prototype.some;
 if (nativeSome) {
   try {
-    nativeSome.call([1], function () {
+    var worksWithStrings;
+    nativeSome.call('abc', function () {
+      worksWithStrings = true;
+      return worksWithStrings;
+    });
+
+    if (worksWithStrings !== true) {
+      throw new Error('failed string');
+    }
+
+    var worksWithArguments;
+    nativeSome.call(argObj, function () {
+      worksWithArguments = true;
+      return worksWithStrings;
+    });
+
+    if (worksWithArguments !== true) {
+      throw new Error('failed arguments');
+    }
+
+    if (documentElement) {
+      var fragment = document.createDocumentFragment();
+      var div = document.createElement('div');
+      fragment.appendChild(div);
+
+      var worksWithDOMElements;
+      nativeSome.call(fragment.childNodes, function () {
+        worksWithDOMElements = true;
+        return worksWithDOMElements;
+      });
+
+      if (worksWithDOMElements !== true) {
+        throw new Error('failed documentElement');
+      }
+    }
+
+    var tests = {
+      // Check node 0.6.21 bug where third parameter is not boxed
+      properlyBoxesNonStrict: true,
+      properlyBoxesStrict: true
+    };
+
+    var isStrict = (function () {
       // eslint-disable-next-line no-invalid-this
-      tests.properlyBoxesStrict = typeof this === 'string';
-    }, 'x');
+      return Boolean(this) === false;
+    }());
+
+    if (isStrict) {
+      nativeSome.call([1], function () {
+      // eslint-disable-next-line no-invalid-this
+        tests.properlyBoxesStrict = typeof this === 'string';
+      }, 'x');
+
+      if (tests.properlyBoxesStrict !== true) {
+        throw new Error('failed properlyBoxesStrict');
+      }
+    }
 
     var fn = [
       'return nativeSome.call("foo", function (_, __, context) {',
@@ -34,13 +86,17 @@ if (nativeSome) {
 
     // eslint-disable-next-line no-new-func
     Function('nativeSome', 'tests', fn)(nativeSome, tests);
-  } catch (e) {
+
+    if (tests.properlyBoxesNonStrict !== true) {
+      throw new Error('failed properlyBoxesNonStrict');
+    }
+  } catch (ignore) {
     nativeSome = null;
   }
 }
 
 var $some;
-if (nativeSome && tests.properlyBoxesNonStrict && tests.properlyBoxesStrict) {
+if (nativeSome) {
   $some = function some(array, callBack /* , thisArg */) {
     var object = toObject(array);
     var args = [assertIsFunction(callBack)];
@@ -54,15 +110,25 @@ if (nativeSome && tests.properlyBoxesNonStrict && tests.properlyBoxesStrict) {
   // ES5 15.4.4.17
   // http://es5.github.com/#x15.4.4.17
   // https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/some
-  var isString = require('is-string');
-  var toLength = require('to-length-x');
-  var isUndefined = require('validate.io-undefined');
   var splitString = require('has-boxed-string-x') === false;
+  var isString = splitString && require('is-string');
+  var hasArgumentsLengthBug = argObj.length !== 1;
+  var isArguments = hasArgumentsLengthBug && require('is-arguments');
+  var toLength = require('to-length-x');
+  var slice = require('array-slice-x');
+  var isUndefined = require('validate.io-undefined');
+
   $some = function some(array, callBack /* , thisArg */) {
     var object = toObject(array);
     // If no callback function or if callback is not a callable function
     assertIsFunction(callBack);
-    var iterable = splitString && isString(object) ? object.split('') : object;
+    var iterable;
+    if ((isString && isString(object)) || (isArguments && isArguments(object))) {
+      iterable = slice(object);
+    } else {
+      iterable = object;
+    }
+
     var length = toLength(iterable.length);
     var thisArg;
     if (arguments.length > 2) {
--- tests/spec/test.js
+++ tests/spec/test.js
@@ -21,6 +21,9 @@ if (typeof module === 'object' && module.exports) {
   some = returnExports;
 }
 
+var documentElement = typeof document !== 'undefined' && document.documentElement;
+var itHasDocumentElement = documentElement ? it : xit;
+
 // IE 6 - 8 have a bug where this returns false.
 var canDistinguish = 0 in [void 0];
 var undefinedIfNoSparseBug = canDistinguish ? void 0 : {
@@ -207,5 +210,31 @@ describe('some', function () {
 
     expect(typeof listArg).toBe('object');
     expect(Object.prototype.toString.call(listArg)).toBe('[object String]');
+  });
+
+  it('should work with arguments', function () {
+    var argObj = (function () {
+      return arguments;
+    }('1'));
+
+    var callback = jasmine.createSpy('callback');
+    some(argObj, callback);
+    expect(callback).toHaveBeenCalledWith('1', 0, argObj);
+  });
+
+  it('should work with strings', function () {
+    var callback = jasmine.createSpy('callback');
+    var string = '1';
+    some(string, callback);
+    expect(callback).toHaveBeenCalledWith('1', 0, string);
+  });
+
+  itHasDocumentElement('should work wih DOM elements', function () {
+    var fragment = document.createDocumentFragment();
+    var div = document.createElement('div');
+    fragment.appendChild(div);
+    var callback = jasmine.createSpy('callback');
+    some(fragment.childNodes, callback);
+    expect(callback).toHaveBeenCalledWith(div, 0, fragment.childNodes);
   });
 });
