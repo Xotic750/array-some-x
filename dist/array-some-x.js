74d14c25a5da553326fedd7a491812b59c488cb6
Graham Fairweather <xotic750@gmail.com>
2019-07-20 00:26:09 +0200

    :bookmark: v3.0.4
    Auto build

--- dist/array-some-x.js
+++ dist/array-some-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-18T22:46:44.328Z",
+  "date": "2019-07-19T22:26:03.398Z",
   "describe": "",
   "description": "Tests whether some element passes the provided function.",
   "file": "array-some-x.js",
   "hash": "5c679d01d45c7eb1f186",
   "license": "MIT",
-  "version": "3.0.3"
+  "version": "3.0.4"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
