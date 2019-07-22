616e0545a1aa9905671c80cd619c039303520abf
Graham Fairweather <xotic750@gmail.com>
2019-07-22 15:14:15 +0200

    :bookmark: v3.0.6
    Dependency updates

--- dist/array-some-x.js
+++ dist/array-some-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-21T23:14:19.460Z",
+  "date": "2019-07-22T13:14:09.433Z",
   "describe": "",
   "description": "Tests whether some element passes the provided function.",
   "file": "array-some-x.js",
   "hash": "5c679d01d45c7eb1f186",
   "license": "MIT",
-  "version": "3.0.5"
+  "version": "3.0.6"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
