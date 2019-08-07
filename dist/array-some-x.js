6a74d817e3c755d070a02d712f9d4e3a0ee999de
Graham Fairweather <xotic750@gmail.com>
2019-08-07 03:46:07 +0200

    Auto build

--- dist/array-some-x.js
+++ dist/array-some-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-06T21:33:15.041Z",
+  "date": "2019-08-07T01:46:01.369Z",
   "describe": "",
   "description": "Tests whether some element passes the provided function.",
   "file": "array-some-x.js",
